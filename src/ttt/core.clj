06ef805d27af1429e931e95a738cd2371a7e24df
Jeff Ramnani <jramnani@8thlight.com>
2016-04-12 15:33:08 -0500

    Can take a spot on the board.

--- src/ttt/core.clj
+++ src/ttt/core.clj
@@ -2,3 +2,6 @@
 
 (defn create-board []
   (vec (repeatedly 9 str)))
+
+(defn take-spot [board player spot]
+  (assoc board spot player))
--- test/ttt/core_test.clj
+++ test/ttt/core_test.clj
@@ -5,4 +5,11 @@
 (deftest board-test
   (testing "Create an empty board."
     (let [test-board (create-board)]
-      (is (= 9 (count test-board))))))
+      (is (= 9 (count test-board)))))
+
+  (testing "Take a spot on the board"
+    (let [test-board (create-board)
+          spot 1
+          player "X"
+          new-board (take-spot test-board player spot)]
+      (is (= player (nth new-board spot))))))
