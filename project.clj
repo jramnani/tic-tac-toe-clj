5036a8d7b053670ab1074b8b3107f11b657261a5
Jeff Ramnani <jramnani@8thlight.com>
2017-12-28 19:52:50 -0600

    Generate JUnit reports

--- project.clj
+++ project.clj
@@ -4,5 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
+  :profiles {:test {:plugins [[test2junit "1.3.3"]]}}
   :aliases { "coverage" ["cloverage" "--ns-exclude-regex" "ttt\\.core"] }
+  :test2junit-output-dir "target/junit"
+  :test2junit-run-ant false
   :main ttt.core)
