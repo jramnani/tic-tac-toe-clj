69deb1878f2c3a67320645a170a99791c5adefc5
Jeff Ramnani <jramnani@users.noreply.github.com>
2018-01-02 13:26:33 -0600

    Remove failing test

--- test/ttt/failing_test.clj
+++
@@ -1,8 +0,0 @@
-(ns ttt.failing-test
-  (:require [clojure.test :refer :all]
-            [ttt.test-helper :refer :all]
-            [ttt.board :refer :all]))
-
-(deftest jenkins-failure
-  (testing "A failing test in Jenkins")
-    (is (= true false)))
